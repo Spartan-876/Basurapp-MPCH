c9679d59dabcbb02b327197ee20975d1a1bbad2a
JohnCh-867 <johnandersonchaponanm@gmail.com>
2026-04-24 23:03:21 -0500

    Carga inicial
    Carga de backend de proyecto inicial con initlzr

---
+++ Backend/src/main/java/com/utp/Basurapp/BasurappApplication.java
@@ -0,0 +1,13 @@
+package com.utp.Basurapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasurappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasurappApplication.class, args);
+	}
+
+}
